24a8a20455194b9af58ab7c2bcded05ee0ce2a9f
a-rasines <alberto.rasines@opendeusto.es>
2021-10-21 16:35:52 +0200

    libGDX añadido

---
+++ ios/src/dg/main/IOSLauncher.java
@@ -0,0 +1,22 @@
+package dg.main;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import dg.main.DokdoCore;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new DokdoCore(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}

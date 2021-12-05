abe179333e2653f010a108986e9a346b8854e37f
EnekoHernando <eneko.hernando@opendeusto.es>
2021-12-05 16:42:00 +0100

    Avance pantalla muerte

--- core/src/dg/main/PantallaMuerte.java
+++ core/src/dg/main/PantallaMuerte.java
@@ -1,11 +1,32 @@
 package dg.main;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-public class PantallaMuerte implements Screen{
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
+public class PantallaMuerte implements Screen{
+	protected Stage stage;
+	protected Skin skin;
+	public PantallaMuerte() {
+		
+	}
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-		
+		Gdx.input.setInputProcessor(stage);
+		Table menu = new Table();
+		menu.setFillParent(true);
+		menu.center();
+		TextButton incio = new TextButton("Jugar de nuevo", skin);
+		TextButton menuPrincipal = new TextButton("Volver al menu", skin);
+		TextButton salir=new TextButton("Salir", skin);
+		menu.add(incio).width(100);
+		menu.row();
+		menu.add(menuPrincipal).width(100);
+		menu.row();
+		menu.add(salir).width(100);
+		stage.addActor(menu);
 	}
 
 	@Override
